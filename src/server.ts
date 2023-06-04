bbfd532b1edb7877f6e9aefd95dcfea3f754a686
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-04 19:38:57 +0600

    12-2: [Fix] change .eslintrs file -> env object key browser to node

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
 import mongoose from 'mongoose';
 import config from './config/index';
 import app from './app';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Color } from 'colors';
 require('colors');
 
@@ -15,7 +20,6 @@ const dbConnect = async () => {
     app.listen(config.port, () => {
       console.log(`Server Up and Running`.cyan.bold);
     });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     console.log(console.log(error.name.bgRed, error.message.bold, error.stack));
   }
