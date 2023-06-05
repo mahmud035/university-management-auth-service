023ab3aa1e89c7c4c2d4536f0b4559d4fa9abb61
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-05 18:42:55 +0600

    12-13: Refactor global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,47 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import config from '../../config/index';
 import { IGenericErrorMessage } from '../../interfaces/error';
+import handleValidationError from '../../errors/handleValidationError';
+import ApiError from '../../errors/ApiError';
 
-const globalErrorHandler = (error: any, req: Request, res: Response) => {
-  const statusCode = 500;
-  const message = 'Something went wrong';
-  const errorMessages: IGenericErrorMessage[] = [];
+const globalErrorHandler = (
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let errorMessages: IGenericErrorMessage[] = [];
+
+  if (error?.name === 'ValidationError') {
+    const simplifyError = handleValidationError(error);
+    // set response according to handleValidationError response
+    statusCode = simplifyError.statusCode;
+    message = simplifyError.message;
+    errorMessages = simplifyError.errorMessages;
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
 
   res.status(statusCode).json({
     success: false,
@@ -13,6 +49,8 @@ const globalErrorHandler = (error: any, req: Request, res: Response) => {
     errorMessages,
     stack: config.env !== 'production' ? error?.stack : undefined,
   });
+
+  next();
 };
 
 export default globalErrorHandler;
