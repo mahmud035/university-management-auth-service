0d4135a246d7106e8556aee9de7ddd116a4c8245
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-06 12:03:24 +0600

    13-4 : What is Zod , Implement one extra layer of validation

--- src/app/modules/Users/user.controller.ts
+++ src/app/modules/Users/user.controller.ts
@@ -1,8 +1,24 @@
 import { RequestHandler } from 'express';
 import { UserService } from './user.services';
+import { z } from 'zod';
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    // req-validation
+    // body ---> object
+    // data ---> object
+
+    const createUserZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: `role is required`,
+        }),
+        password: z.string().optional(),
+      }),
+    });
+
+    await createUserZodSchema.parseAsync(req);
+
     const { user } = req.body;
     const result = await UserService.createUser(user);
 
