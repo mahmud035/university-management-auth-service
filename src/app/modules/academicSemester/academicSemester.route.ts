55c4d9ad66c74461071994e07798e3bfe184dda3
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-10 06:36:13 +0600

    13-8 Create Academic Semester Validation ,Improve interface , schema.

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { AcademicSemesterValidation } from './academicSemester.validation';
+import validateRequest from '../../middlewares/validateRequest';
+const router = express.Router();
+
+router.post(
+  '',
+  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema)
+);
