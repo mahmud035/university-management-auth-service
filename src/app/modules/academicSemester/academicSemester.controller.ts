23036b5dfd57e5e2f820abd924a2ff7ea0d1747a
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-14 13:16:43 +0600

    14-4: Implement Pagination using page , limit and skip

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,6 +3,9 @@ import { AcademicSemesterService } from './academicSemester.services';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
+import { IAcademicSemester } from './academicSemester.interface';
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -11,7 +14,7 @@ const createSemester = catchAsync(
       academicSemesterData
     );
 
-    sendResponse(res, {
+    sendResponse<IAcademicSemester>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Academic semester is created successfully',
@@ -22,6 +25,25 @@ const createSemester = catchAsync(
   }
 );
 
+const getAllSemesters = catchAsync(async (req: Request, res: Response) => {
+  const paginationOptions = pick(req.query, paginationFields);
+
+  console.log(paginationOptions);
+
+  const result = await AcademicSemesterService.getAllSemesters(
+    paginationOptions
+  );
+
+  sendResponse<IAcademicSemester[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semesters retrieved successfully!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicSemesterController = {
   createSemester,
+  getAllSemesters,
 };
---
+++ src/shared/pick.ts
@@ -0,0 +1,25 @@
+// ['page', 'limit', 'sortBy', 'sortOrder']
+
+const pick = <T extends Record<string, unknown>, k extends keyof T>(
+  obj: T,
+  keys: k[]
+): Partial<T> => {
+  const finalObj: Partial<T> = {};
+
+  for (const key of keys) {
+    if (obj && Object.hasOwnProperty.call(obj, key)) {
+      finalObj[key] = obj[key];
+    }
+  }
+
+  return finalObj;
+};
+
+export default pick;
+
+// const paginationOptions = {
+//   page: Number(req.query.page),
+//   limit: Number(req.query.limit),
+//   sortBy: req.query.sortBy,
+//   sortOrder: req.query.sortOrder,
+// };
